d6d99e6bf230664687cb5dbdce16580aa380acf0
DungTranManh <manhdung5801@gmail.com>
2022-10-15 11:25:01 +0700

    build profile tab

--- app.py
+++ app.py
@@ -35,7 +35,7 @@ def index():
     if "dangnhapthanhcong" in session:
         return redirect(url_for("mainpage", username = session['username']))
     else:
-        return redirect(url_for('login', alert = "none"))
+        return redirect(url_for('login', alert = " "))
 
 @app.route("/admin/<username>/", methods=["GET","POST"])
 def mainpage(username):
@@ -57,6 +57,8 @@ def login(alert):
                     if find_user.is_admin == 1:
                         session['dangnhapthanhcong'] = 'OK'
                         session['username'] = find_user.username
+                        session['email'] = find_user.email
+                        session['password'] = get_password
                         # return render_template('index.html', username_login = find_user.username)
                         return redirect(url_for("mainpage", username = session['username']))
                     else:
@@ -91,9 +93,38 @@ def register():
 def logout():
     session.pop("dangnhapthanhcong", None)
     session.pop("username", None)
-    return redirect(url_for('login', alert = "/"))
+    return redirect(url_for('login', alert = " "))
 
-
+@app.route("/admin/profile/", methods=["GET","POST"])
+def profile():
+    
+    if "dangnhapthanhcong" in session:
+        email = session['email'] 
+        password = session['password'] 
+        username = session["username"]
+        context = [email,password]
+        if request.method == "POST":
+            get_username_modify = request.form["UsernameModify"]
+            get_email_modify = request.form['EmailModify']
+            get_password_modify = request.form["PasswordModify"]
+            get_confirm_password_modify = request.form["ConfirmPasswordModify"]
+            # Check email
+            if get_email_modify != email:
+                user_already_check = User.query.filter_by(email=get_email_modify).first()
+                # check xem đã tồn tại user có email bằng email_modify hay chưa?
+                if user_already_check:
+                    # show alert email đã tồn tại
+                    pass
+                else:
+                    user_modify = User.query.filter_by(email=email).first()
+                    user_modify.email = get_email_modify
+                    session["email"] = get_email_modify
+                    db.session.commit()
+                    return redirect(url_for("profile"))
+            return render_template("profile.html", context = context, alert = "done", username_login = username)
+        return render_template("profile.html", context = context, username_login = username )
+    else:
+        return redirect(url_for('login', alert = "session_expired"))
 
 if __name__ == "__main__":
     app.run(debug=True, host='localhost', port=8000)
