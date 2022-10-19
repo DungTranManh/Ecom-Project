bcfdb73717dfde77d78f311af60aeb9a4248cced
DungTranManh <manhdung5801@gmail.com>
2022-10-19 22:32:45 +0700

    update modal to confirm delete account

--- static/js/showAccContent.js
+++ static/js/showAccContent.js
@@ -1,19 +1,38 @@
-var url_getalluserAPI = 'http://127.0.0.1:8000/api/getallusers'
+var url_getalluserAPI = 'http://127.0.0.1:8000/api/getallusers';
+var modal = document.getElementById('id01');
+
 function start() {
     GetAllAcc(function(allAccs){
         RenderAllAcc(allAccs);
     });
-    var modal = document.getElementById('id01');
+    
+}
+start();
 
+
+function showModal(user_id, UsernameDelete) {
     // When the user clicks anywhere outside of the modal, close it
     window.onclick = function(event) {
         if (event.target == modal) {
             modal.style.display = "none";
         }
     }
+    document.getElementById('id01').style.display='block';
+    document.querySelector(".deletebtn").addEventListener("click", function(){
+        fetch('http://127.0.0.1:8000/api/account/' + user_id, {
+            method: 'DELETE',
+        })
+        .then(res => console.log(res))
+        .then(function(){
+            var itemDeleted = document.querySelector("#tr-" +user_id);
+            if(itemDeleted){
+                itemDeleted.remove();
+            } 
+        })
+        document.getElementById('id01').style.display='none';
+    });
 }
-start();
-
+    
 
 // Fetch all account and render a view
 function GetAllAcc(callback) {
@@ -24,26 +43,27 @@ function GetAllAcc(callback) {
 
 function RenderAllAcc(allAccs) {
     var ContentOfTable = document.querySelector("#content-of-table");
-    var html = allAccs.map(function(allAcc){
+    var html1 = allAccs.map(function(allAcc){
         if (allAcc.is_admin == 1){
             allAcc.is_admin = "admin";
         }else{
             allAcc.is_admin = "member";
         }
         return `
-        <tr>
+        <tr id = "tr-${allAcc.user_id}">
             <th scope="row" class="text-center">${allAcc.user_id}</th>
             <td align ="center">${allAcc.username}</td>
             <td align ="center">${allAcc.email}</td>
             <td align ="center">${allAcc.is_admin}</td>
             <td align ="center">
                 <button type="button" class="btn btn-square btn-outline-warning m-2" ><i class="fas fa-user-edit"></i></button>
-                <button type="button" class="btn btn-square btn-outline-danger m-2" onclick="document.getElementById('id01').style.display='block'""><i class="fas fa-user-minus"></i></button>
+                <button type="button" class="btn btn-square btn-outline-danger m-2" onclick="showModal(${allAcc.user_id},'${allAcc.username}');" id = "${allAcc.user_id}""><i class="fas fa-user-minus"></i></button>
             </td>
         </tr>
         `;
     });
-    ContentOfTable.innerHTML = html.join("");
+
+    ContentOfTable.innerHTML = html1.join("");   
 }
 
 
